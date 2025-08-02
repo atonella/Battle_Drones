a7d42e63ea13eac656029c4b508f0c6d484e77a1
atonella <anthonytonella@gmail.com>
2025-08-02 15:23:26 +0200

    fix (#85)

--- source/cartridge.c
+++ source/cartridge.c
@@ -15,7 +15,7 @@ const struct cartridge_t game_header __attribute__((section(".cartridge"), used)
 	.title_height = -8,
 	.title_width = 80,
 	.title_y = 24,
-	.title_x = CENTER_TEXT(TEXT_WIDTH_L, 13),
+	.title_x = CENTER_TEXT(TEXT_WIDTH_L, 14),
 	.title = "VECTROCOPTERS\x80" // note that \x00 is automatically appended!
 };
 
